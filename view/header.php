3b1b0105de6f7f2f908981a397251bb4def7ceda
DamThanh-Phuong <phuongdt.23it@vku.udn.vn>
2024-09-09 00:17:28 +0700

    P_9/9

---
+++ view/header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"/>
+    <link rel="stylesheet" href="../css/header_footer.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hurricane&family=Ibarra+Real+Nova:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+    <title>CoVietPhuc_header</title>
+</head>
+<body id="header">
+    <hr color="yellow">
+    <hr color="yellow">
+
+    <h1 align="center" valign="middle">Cổ phục Việt</h1>
+
+    <div class="left">       
+        <h4 id="home">HOME</h4>
+        <h4 id="clothing">ANCIENT CLOTHING</h4>
+        <h4 id="about">ABOUT US</h4>
+    </div>
+
+    <div class="right">       
+        <h4 id="register">SIGN IN/REGISTER</h4>
+        <h4 id="contact">CONTACT</h4>
+    </div>
+
+    <i class="fa-duotone fa-solid fa-bars fa-xl" style="color: #f8fc03;" id="menu"></i>
+
+
+    <div class=container_img>
+        <img src="../img/logo.png" alt="logo" id="logo" align="center" valign="middle">
+    </div>
+    <hr color="yellow" id="hr1">
+    <hr color="yellow">
+
+    <div class="bottom">
+        <i class="fa-brands fa-facebook-f fa-lg fa-fade" style="color: blue"></i>
+        <i class="fa-brands fa-instagram fa-lg fa-fade" style="color: purple;"></i>
+        <i class="fa-brands fa-tiktok fa-lg fa-fade" style="color: brown;"></i>
+        <i class="fa-brands fa-youtube fa-lg fa-fade" style="color: red;"></i>
+    </div>
+    
+    <h5>(+84) 123456789</h5> 
+
+</body>
+</html>
