2f7f9e61b0a342f6f094eb6e72e8b304e5d2da68
DamThanh-Phuong <phuongdt.23it@vku.udn.vn>
2024-09-10 00:36:44 +0700

    00:36

--- view/footer.php
+++ view/footer.php
@@ -5,14 +5,64 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/header_footer.css"/>
-    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"/>
+    <link rel="stylesheet" href="../css/footer.css"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hurricane&family=Ibarra+Real+Nova:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+    <title>CoPhucViet_footer</title>
 </head>
-<body id="footer">
-    <header id="header">
-        <?php
-            include '../view/header.php';
-        ?>
-    </header>  
-    <h2>Hello footer</h2>
+<body>
+    <footer>
+        <div class="left">
+            <img src="../img/logo.png" alt="logo_right" class="logo_right">
+            <h2>Cổ phục Việt</h2>
+            <h2 id="ON">O&N</h2>
+            <h5>DuongDomic Duong D-O-M-I-C Wean Le Weantodale</h5>
+            <h5>Quang Hung MasterD Negav </h5>
+            <div id="icon1">
+                <i class="fa-brands fa-facebook-f fa-lg" style="color: yellow"></i>
+                <i class="fa-brands fa-instagram fa-lg" style="color: yellow;"></i>
+                <i class="fa-brands fa-tiktok fa-lg" style="color: yellow;"></i>
+                <i class="fa-brands fa-youtube fa-lg" style="color: yellow;"></i>
+            </div>         
+        </div>
+
+        <div class="right1">
+            <h3>Our store</h3>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Service</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+
+        <div class="right2">
+            <h3>Get in touch</h3>
+            <div class="contact-container">
+                <div id="icon">
+                    <i class="fa-solid fa-map-location-dot" style="color: #FFD43B;"></i>
+                    <i class="fa-solid fa-phone" style="color: #FFD43B;"></i>      
+                    <i class="fa-solid fa-envelope" style="color: #FFD43B;"></i>      
+                </div>
+                <div id="information">
+                    <h4>470 Tran Dai Nghia, Ngu Hanh Son, Da Nang, Viet Nam</h4>                
+                    <h4>(+84) 123456789</h4>               
+                    <h4>CPV@gmail.com</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class=container_img>
+            <img src="../img/logo.png" alt="logo_left">
+        </div>
+
+        <hr color="yellow">
+        <p align="center" valign="middle">
+            &copy;<?php echo date("Y"); ?>CoPhucViet | DucNguyenPhuong
+        </p>
+      
+    </footer> 
 </body>
 </html>
--- view/header.php
+++ view/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"/>
-    <link rel="stylesheet" href="../css/header_footer.css"/>
+    <link rel="stylesheet" href="../css/header.css"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Hurricane&family=Ibarra+Real+Nova:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
@@ -39,7 +39,7 @@
         <div class="bottom">
             <i class="fa-brands fa-facebook-f fa-lg fa-fade" style="color: blue"></i>
             <i class="fa-brands fa-instagram fa-lg fa-fade" style="color: purple;"></i>
-            <i class="fa-brands fa-tiktok fa-lg fa-fade" style="color: brown;"></i>
+            <i class="fa-brands fa-tiktok fa-lg fa-fade" style="color: black;"></i>
             <i class="fa-brands fa-youtube fa-lg fa-fade" style="color: red;"></i>
         </div>
         
