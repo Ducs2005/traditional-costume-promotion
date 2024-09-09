64881d90a5da7d8ef12790b3c9aec94a59789ab7
Pham Ngoc Duc <ducp4308@gmail.com>
2024-09-09 21:37:16 +0700

    Add banner and basic js

---
+++ home_view.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home view</title>
+
+    <!-- Include CSS file -->
+    <link rel="stylesheet" href="css/header_footer.css">
+</head>
+<?php
+    include("view/header.php")
+?>
+<br>
+<br>
+<br>
+
+<body>
+    <?php include("view/banner.php") ?>
+    
+</body>
+</html>
+
+
+<br>
+<?php
+    //include("view/footer.php")
+?>
+
+
+
---
+++ view/banner.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="css/banner.css">
+<div class="banner">
+        <img src="img/banner1.jpg" alt="Banner Image">
+        <div class="banner-content">
+            <h1>Welcome to My Website</h1>
+            <p>Providing the best services for your needs</p>
+            <a href="#services" class="btn">Learn More</a>
+        </div>
+</div>
+<script src="js/banner.js"></script>
+
