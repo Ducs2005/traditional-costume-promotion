596684901b11e19ee5e5adfc97c4c9c8fc54b702
DamThanh-Phuong <phuongdt.23it@vku.udn.vn>
2024-09-09 11:58:53 +0700

    11:58

--- view/header.php
+++ view/header.php
@@ -10,40 +10,40 @@
     <link href="https://fonts.googleapis.com/css2?family=Hurricane&family=Ibarra+Real+Nova:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
     <title>CoVietPhuc_header</title>
 </head>
-<body id="header">
-    <hr color="yellow">
-    <hr color="yellow">
+<body>
+    <header>
+        <hr color="black" id="hr1">
+        <hr color="yellow">
 
-    <h1 align="center" valign="middle">Cổ phục Việt</h1>
+        <h1 align="center" valign="middle">Cổ phục Việt</h1>
 
-    <div class="left">       
-        <h4 id="home">HOME</h4>
-        <h4 id="clothing">ANCIENT CLOTHING</h4>
-        <h4 id="about">ABOUT US</h4>
-    </div>
+        <div class="left">       
+            <h4 id="home">HOME</h4>
+            <h4 id="clothing">ANCIENT CLOTHING</h4>
+            <h4 id="about">ABOUT US</h4>
+        </div>
 
-    <div class="right">       
-        <h4 id="register">SIGN IN/REGISTER</h4>
-        <h4 id="contact">CONTACT</h4>
-    </div>
+        <div class="right">       
+            <h4 id="register">SIGN IN/REGISTER</h4>
+            <h4 id="contact">CONTACT</h4>
+        </div>
 
-    <i class="fa-duotone fa-solid fa-bars fa-xl" style="color: #f8fc03;" id="menu"></i>
+        <i class="fa-duotone fa-solid fa-bars fa-xl" style="color: #f8fc03;" id="menu"></i>
 
+        <div class=container_img>
+            <img src="../img/logo.png" alt="logo">
+        </div>
+        <hr color="yellow">
+        <hr color="black"  id="hr1">
 
-    <div class=container_img>
-        <img src="../img/logo.png" alt="logo" id="logo" align="center" valign="middle">
-    </div>
-    <hr color="yellow" id="hr1">
-    <hr color="yellow">
-
-    <div class="bottom">
-        <i class="fa-brands fa-facebook-f fa-lg fa-fade" style="color: blue"></i>
-        <i class="fa-brands fa-instagram fa-lg fa-fade" style="color: purple;"></i>
-        <i class="fa-brands fa-tiktok fa-lg fa-fade" style="color: brown;"></i>
-        <i class="fa-brands fa-youtube fa-lg fa-fade" style="color: red;"></i>
-    </div>
-    
-    <h5>(+84) 123456789</h5> 
-
+        <div class="bottom">
+            <i class="fa-brands fa-facebook-f fa-lg fa-fade" style="color: blue"></i>
+            <i class="fa-brands fa-instagram fa-lg fa-fade" style="color: purple;"></i>
+            <i class="fa-brands fa-tiktok fa-lg fa-fade" style="color: brown;"></i>
+            <i class="fa-brands fa-youtube fa-lg fa-fade" style="color: red;"></i>
+        </div>
+        
+        <h5>(+84) 123456789</h5> 
+    </header>
 </body>
 </html>
