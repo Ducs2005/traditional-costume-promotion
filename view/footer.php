401106e6c62cbd195df595d77dd043a3412f5c8d
DamThanh-Phuong <phuongdt.23it@vku.udn.vn>
2024-09-14 01:11:49 +0700

    header-footer

--- view/footer.php
+++ view/footer.php
@@ -1,68 +1,60 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/footer.css"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"/>
-    <link rel="stylesheet" href="css/footer.css"/>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://fonts.googleapis.com/css2?family=Hurricane&family=Ibarra+Real+Nova:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
-    <title>CoPhucViet_footer</title>
+    <link rel="stylesheet" href="css/footer.css">
+    <title>Footer</title>
 </head>
 <body>
     <footer>
-        <div class="left_footer">
-            <img src="img/logo.png" alt="logo_right" class="logo_right">
-            <h2>Cổ phục Việt</h2>
-            <h2 id="ON">O&N</h2>
-            <h5>DuongDomic Duong D-O-M-I-C Wean Le Weantodale</h5>
-            <h5>Quang Hung MasterD Negav </h5>
-            <div id="icon1">
-                <i class="fa-brands fa-facebook-f fa-lg" style="color: yellow"></i>
-                <i class="fa-brands fa-instagram fa-lg" style="color: yellow;"></i>
-                <i class="fa-brands fa-tiktok fa-lg" style="color: yellow;"></i>
-                <i class="fa-brands fa-youtube fa-lg" style="color: yellow;"></i>
-            </div>         
-        </div>
-
-        <div class="right1">
-            <h3>Our store</h3>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Service</li>
-                <li>Contact</li>
-            </ul>
-        </div>
-
-        <div class="right2">
-            <h3>Get in touch</h3>
-            <div class="contact-container">
-                <div id="icon">
-                    <i class="fa-solid fa-map-location-dot" style="color: #FFD43B;"></i>
-                    <i class="fa-solid fa-phone" style="color: #FFD43B;"></i>      
-                    <i class="fa-solid fa-envelope" style="color: #FFD43B;"></i>      
-                </div>
-                <div id="information">
-                    <h4>470 Tran Dai Nghia, Ngu Hanh Son, Da Nang, Viet Nam</h4>                
-                    <h4>(+84) 123456789</h4>               
-                    <h4>CPV@gmail.com</h4>
+        <div class="row">
+            <div class="col">
+                <img src="img/logo.png" class="logo">
+                <p>Anh trai say Hi Kim phut kim gio Chan thanh</p>
+                <p>Duong Domic Yeu em 2 ngay - Weantodale Badbye</p>
+                <p>Oh Hae Young</p>
+            </div>
+            <div class="col">
+                <h3>Get in touch <div class="underline"><span></span></div> </h3>
+                <p>470 Tran Dai Nghia, Ngu Hanh Son, Da Nang, Viet Nam</p>           
+                <p class="email-id">CPV@gmail.com</p>
+                <p>(+84) 123456789</p>
+            </div>
+            <div class="col">
+                <h3 class="contact">Contact <div class="underline"><span></span></div> </h3>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Features</a></li>
+                    <li><a href="#">Contacts</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h3>Newletter <div class="underline"><span></span></div> </h3>
+                <form class="mail">
+                    <i class='bx bx-envelope'></i>
+                    <input type="email" placeholder="Enter your email to receive notifications." class="email">
+                    <button class="submit"><i class="fas fa-arrow-right"></i></button>
+                </form>
+                <div class="social-icons">
+                    <i class="fab fa-facebook"></i>
+                    <i class="fab fa-tiktok"></i>
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-youtube"></i>
                 </div>
             </div>
         </div>
 
-        <div class=container_img_footer>
-            <img src="img/logo.png" alt="logo_left">
-        </div>
-
-        <hr color="yellow" id="hr_footer">
-        <p align="center" valign="middle">
+        <hr>
+        <p class="copyright">
             &copy;<?php echo date("Y"); ?>CoPhucViet | DucNguyenPhuong
         </p>
-      
-    </footer> 
+        
+    </footer>
 </body>
 </html>
--- view/header.php
+++ view/header.php
@@ -1,52 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"/>
-        <link rel="stylesheet" type="text/css" href="../css/header.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Hurricane&family=Ibarra+Real+Nova:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
-        <title>CoVietPhuc_header</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=Hurricane&family=Ibarra+Real+Nova:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/header.css">
+    <title>Header</title>
 </head>
-
 <body>
     <header>
-        <!-- <h1 align="center" valign="middle">Cổ phục Việt</h1> -->
-        <div class="header">
-
-            <hr color="black" id="hr1">
-            <hr color="yellow">
-
-            <div class="left_header">       
-                <h4 id="home">HOME</h4>
-                <h4 id="clothing">ANCIENT CLOTHING</h4>
-                <h4 id="about">ABOUT US</h4>
-            </div>
-
-            <div class="right">       
-                <h4 id="register">SIGN IN/REGISTER</h4>
-                <h4 id="contact">CONTACT</h4>
-            </div>
-
-            <div class=container_img_header align="center" valign="middle">
-                <img src="../img/logo.png" alt="logo">
-            </div>
-
-            <i class="fa-duotone fa-solid fa-bars fa-xl" id="menu"></i>
+        <div class="logo1"><img src="img/logo.png" alt=""></div>
+        <div class="search-box">
+            <form class="search">
+                <input type="text" name="search" id="srch" placeholder="Search">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
         </div>
-        
-        <hr color="yellow">
-        <hr color="black"  id="hr1">
-
-        <div class="bottom">
-            <i class="fa-brands fa-facebook-f fa-lg fa-fade" style="color: blue"></i>
-            <i class="fa-brands fa-instagram fa-lg fa-fade" style="color: purple;"></i>
-            <i class="fa-brands fa-tiktok fa-lg fa-fade" style="color: black;"></i>
-            <i class="fa-brands fa-youtube fa-lg fa-fade" style="color: red;"></i>
-            <i  class="h5_header"><h5>(+84) 123456789</h5></i> 
-        </div>       
-    </header> 
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Acient Clothing</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="account/login_register.php">Log in</a></li>
+            <li><a href="#">Contact</a></li>
+            <li>
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-tiktok"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+            </li>
+        </ul>
+        <div class="menu">
+            <label for="mn">
+                <i class="fa fa-bars"></i>
+            </label>
+        </div>
+    </header>
 </body>
 </html>
