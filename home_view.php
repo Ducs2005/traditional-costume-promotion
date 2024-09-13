f9b5f43ed97f3f7cb97babe27d604c2600a0b461
Pham Ngoc Duc <ducp4308@gmail.com>
2024-09-13 11:03:54 +0700

    tempo commit

--- home_view.php
+++ home_view.php
@@ -29,7 +29,7 @@
 
 
 <?php
-    include("view/footer.php")
+    //include("view/footer.php")
 ?>
 
 
--- view/product_list.php
+++ view/product_list.php
@@ -1,7 +1,54 @@
 <link rel="stylesheet" href="css/product_list.css">
 <link rel="stylesheet" href="css/main.css">
+
 <div class="product-list">
     <div class="tag-container">
         Nổi bật nhất
+    </div>
+</div>
+
+<div class="product-container">
+    <div class="container">
+    <div class="product-item">
+                <img src="img/product/img1.jpg" alt="Product 1">
+                <h4>Áo Dài Ngũ Thân Tay Chẵn học sinh vải lụa văn cho nữ</h4>
+                <p>690.000₫</p>
+            </div>
+            <!-- Product 2 -->
+            <div class="product-item">
+                <img src="img/product/img2.jpg" alt="Product 2">
+                <h4>Áo ngũ thân cách tân viền lục giác (Unisex)</h4>
+                <p>390.000₫</p>
+            </div>
+            <!-- Product 3 -->
+            <div class="product-item">
+                <img src="img/product/img3.jpg" alt="Product 3">
+                <h4>Áo Ngũ Thân cách tân Xuân Hiểu</h4>
+                <p>890.000₫</p>
+            </div>
+    </div>
+
+    <div class="sidebar">
+    <h3>DANH MỤC SẢN PHẨM</h3>
+            <form>
+                <input type="checkbox" id="cach-tan" name="category">
+                <label for="cach-tan">Trang phục cách tân</label><br>
+                
+                <input type="checkbox" id="men" name="category">
+                <label for="men">Nam</label><br>
+
+                <input type="checkbox" id="women" name="category">
+                <label for="women">Nữ</label><br>
+
+                <input type="checkbox" id="children" name="category">
+                <label for="children">Trẻ Em</label><br>
+
+                <input type="checkbox" id="traditional" name="category">
+                <label for="traditional">Trang phục truyền thống</label><br>
+
+                <input type="checkbox" id="rental" name="category">
+                <label for="rental">Trang phục cho thuê</label><br>
+            </form>
+        
     </div>
 </div>
