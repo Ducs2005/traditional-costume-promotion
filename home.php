17ef57a88e4219e0a8cdc1091ad1254eea1ad330
DamThanh-Phuong <phuongdt.23it@vku.udn.vn>
2024-09-21 16:07:17 +0700

    disply_login

--- home.php
+++ home.php
@@ -3,12 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="./css/home.css">
     <title>Home</title>
 </head>
 <body>
     <?php include("./view/header.php"); ?>
-    <?php include("homepage.html"); ?>
+
+    <section class="hero-section">
+        
+        <div class="content">
+            <h1>Welcome to the Vietnamese traditional custome </h1>
+            <p>
+                A traditional clothing website showcases the beauty and heritage of cultural garments from around the world. 
+                It highlights unique styles, craftsmanship, and traditions, offering a glimpse into each culture's identity. 
+                Explore and appreciate the diversity of timeless attire through this platform.
+            </p>
+            <button>Explore</button>
+        </div>
+        <div class="banner"></div>
+            <img src="./img/img.png" alt="">
+        </div>
+    </section>
 
 </body>
 </html>
--- view/header.php
+++ view/header.php
@@ -1,3 +1,8 @@
+<?php 
+    if (!isset($_SESSION) || session_id()=="" || session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,41 +15,52 @@
     <title>Header</title>
 </head>
 <body>
-    <header>
+    <header>        
         <input type="checkbox" name="" id="chk1">
         <div class="logo"><img src="img/logo.png"></div>
+            
         <div class="search-box">
-            <form action="">
+            <form action="#" method="post">
                 <input type="text" name="search" id="srch" placeholder="Search">
                 <button type="submit"><i class="fa fa-search"></i></button>
             </form>
         </div>
-        <ul>
-            <li><a href="#">Trang chủ</a></li>
-            <li><a href="product_description.php">Cổ phục</a>
-                <ul class="sub-menu">
-                    <li><a href="">Áo giao lĩnh</a></li>
-                    <li><a href="">Áo viên lĩnh</a></li>
-                    <li><a href="">Áo đối khâm</a></li>
+        <!-- <div class="menubar">
+            <?php if (isset($_SESSION['name'])){ ?>
+            <li><span><?= htmlspecialchars($_SESSION['name']); ?></span></li> -->
+                <!-- <ul class="sub-menu">
+                    <li><a href="../account/logout.php">Logout</a></li>
+                </ul> -->
+            <!-- <?php   }
+            else { ?> -->
+                <ul>
+                    <li><a href="#">Trang chủ</a></li>
+                    <li><a href="product_description.php">Cổ phục</a>
+                        <ul class="sub-menu">
+                            <li><a href="">Áo giao lĩnh</a></li>
+                            <li><a href="">Áo viên lĩnh</a></li>
+                            <li><a href="">Áo đối khâm</a></li>
+                        </ul>
+                
+                
+                    </li>
+                    <li><a href="#">Về chúng tôi</a></li>         
+                    <li><a href="#">Liên lạc</a></li>
+                    <li><a href="./account/login.php">Đăng nhập</a></li>
+                    <li>
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-tiktok"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                    </li>
                 </ul>
-        
-        
-            </li>
-            <li><a href="#">Về chúng tôi</a></li>         
-            <li><a href="#">Liên lạc</a></li>
-            <li><a href="account/login_register.php">Đăng nhập</a></li>
-            <li>
-                <a href="#"><i class="fab fa-facebook"></i></a>
-                <a href="#"><i class="fab fa-tiktok"></i></a>
-                <a href="#"><i class="fab fa-instagram"></i></a>
-                <a href="#"><i class="fab fa-youtube"></i></a>
-            </li>
-        </ul>
-        <div class="menu">
-            <label for="chk1">
-                <i class="fa fa-bars"></i>
-            </label>
-        </div>
+                <div class="menu">
+                    <label for="chk1">
+                        <i class="fa fa-bars"></i>
+                    </label>
+                </div>
+            <!-- <?php } ?>
+        </div>        -->
     </header>
 </body>
 </html>
