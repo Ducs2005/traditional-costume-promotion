0aa2fa8e8198577f8c56f953033d0260f64564de
DamThanh-Phuong <phuongdt.23it@vku.udn.vn>
2024-09-28 11:34:09 +0700

    update

---
+++ home_view.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home view</title>
+
+    <!-- Include CSS file -->
+    <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+    <?php
+        include("view/header.php");
+    ?>
+    
+    <br><br><br><br>
+
+    <?php 
+        include("view/banner.php"); 
+    ?>
+
+    <br><br><br>
+
+    <?php
+        include("view/product_list.php");
+    ?>
+
+    <br><br><br>
+
+   
+
+    <?php
+        include("view/footer.php");
+    ?>
+
+   
+<!-- Include the products.js script -->
+<script src="js/products.js"></script>
+
+</body>
+
+
+</html>
