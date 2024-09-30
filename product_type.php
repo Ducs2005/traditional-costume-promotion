0fd6ab488e5bc855a7cdd24d5e217d06f92d3a88
hoangnguyenvku000 <161429688+hoangnguyenvku000@users.noreply.github.com>
2024-09-30 13:43:29 +0700

    Update product_type.php

--- product_type.php
+++ product_type.php
@@ -12,26 +12,25 @@
 </head>
 <body>
     <?php include('view/header.php') ?>
-    <?php include('view/chat_window.php') ?>
     <section class="Category">
         <div class="container">
             <h1>Danh Mục Sản Phẩm</h1>
             <div class="product-grid">
                 <div class="product-card">
                     <img src="img//product/giaolinhnam.jpg" alt="Sản phẩm 1">
-                    <h2>Áo Giao Lĩnh</h2>
+                    <h2>Áo giao lĩnh</h2>
                     <p>Áo giao lĩnh là áo có phần cổ giao nhau ở trước ngực, vạt trái đè lên vạt phải, tay áo chủ yếu là loại tay thụng và tay hẹp. Mang đậm âm hưởng văn hóa Đông Á nhưng áo giao lĩnh Việt Nam vẫn có nét riêng là hai vạt váy trong và ngoài không bằng nhau. Vậy nên đây là trang phục mang đậm bản sắc văn hóa Việt.</p>
                     <a href="giaolinh.php" class="btn">Xem Chi Tiết</a>
                 </div>
                 <div class="product-card">
                     <img src="img//product/vienlinhnam.jpg" alt="Sản phẩm 2">
-                    <h2>Áo Viên Lĩnh</h2>
+                    <h2>Áo viên lĩnh</h2>
                     <p>Áo viên lĩnh còn gọi là áo cổ tròn, cổ kiềng… gài cúc bên vai phải. Dạng áo này tương tự như giao lĩnh, nhưng áo là dạng cổ tròn thay vì cổ giao nhau, cũng có dạng vạt ngắn và vạt dài. Vào thời Lê, Nguyễn áo viên lĩnh là kiểu áo chính thức nhất của tầng lớp quan viên và triều đình.</p>
                     <a href="vienlinh.php" class="btn">Xem Chi Tiết</a>
                 </div>
                 <div class="product-card">
                     <img src="img//product/doikhamnu.jpg" alt="Sản phẩm 3">
-                    <h2>Áo Đối Khâm</h2>
+                    <h2>Áo đối khâm</h2>
                     <p>Đối khâm tức vạt áo đối nhau, đối khâm là dạng áo có hai vạt trước đặt song song nhau, thường để buông thõng, khoác bên ngoài, nhìn được rõ lớp áo bên trong. Loại áo này thường xẻ tà 2 bên, độ dài chưa chạm đến chân váy. Từ thời Lý – Trần nước ta đã chuộng lại áo này.</p>
                     <a href="doikham.php" class="btn">Xem Chi Tiết</a>
                 </div>
@@ -49,8 +48,26 @@
                 </div>
                 <div class="product-card">
                     <img src="img//product/aotacnu.jpg" alt="Sản phẩm 6">
-                    <h2>Áo ngũ thân, áo tấc</h2>
+                    <h2>Áo tấc, ngũ thân</h2>
                     <p>Áo ngũ thân được chắp từ năm mảnh vải, che từ cổ đến hoặc quá đầu gối, cổ đứng cài cúc bên phải, thường mặc với quần dài. Từ thời Nguyễn, áo ngũ thân có hai loại: áo tấc và ngũ thân tay chẽn. Áo tấc là áo ngũ thân tay thụng. Còn áo ngũ thân tay chẽn có thân áo như áo tấc nhưng được may kiểu ống tay hẹp.</p>
+                    <a href="product6-details.html" class="btn">Xem Chi Tiết</a>
+                </div>
+                <div class="product-card">
+                    <img src="img//product/nhatbinhhoanghau.jpg" alt="Sản phẩm 7">
+                    <h2>Áo nhật bình</h2>
+                    <p>Áo Nhật bình vốn là loại triều phục dành cho bậc hậu, phi, cung tần và công chúa thời Nguyễn, sau đấy trở thành loại trang phục phổ biến của phụ nữ quý tộc và cả thường dân trong dịp hôn lễ và ngày càng được các bạn nữ trẻ Việt Nam yêu thích sử dụng khi đi ngoạn cảnh.</p>
+                    <a href="product4-details.html" class="btn">Xem Chi Tiết</a>
+                </div>
+                <div class="product-card">
+                    <img src="img//product/tuthan.jpg" alt="Sản phẩm 8">
+                    <h2>Áo tứ thân</h2>
+                    <p>Chiếc áo tứ thân được cấu tạo bởi phần lưng áo gồm hai mảnh vải cùng gam màu ghép lại với nhau, phía trước có hai thân tách rời ra và được buộc lại với nhau, thả trước bụng để tạo sự mềm mại và uyển chuyển khi mặc. Phía trên phần ngực không gài hết mà để lộ chiếc yếm thắm ẩn ở bên trong.</p>
+                    <a href="product5-details.html" class="btn">Xem Chi Tiết</a>
+                </div>
+                <div class="product-card">
+                    <img src="img//product/bantinu.jpg" alt="Sản phẩm 9">
+                    <h2>Bán tí</h2>
+                    <p>Bán tí là một dạng hình thức ống tay áo trong hệ thống y phục vùng văn hóa Đông Á trong đó có Việt Nam. Cái đặc thù nằm ở phần ống tay áo: thường chỉ có độ dài từ cùi trỏ trở lên, thậm chí là không có ống tay(còn gọi là bối tâm). Những dạng thức áo chỉ cần có ống tay ngắn như trên được xem là bán tí.</p>
                     <a href="product6-details.html" class="btn">Xem Chi Tiết</a>
                 </div>
             </div>
